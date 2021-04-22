0b04753f23b28c7dc8b7558ad3f0c346f214167f
tikseyranyan@gmail.com <tikseyranyan@gmail.com>
2021-04-22 23:01:15 +0400

    added sub method

--- src/main/java/firstlesson/TestClass.java
+++ src/main/java/firstlesson/TestClass.java
@@ -3,15 +3,26 @@ package firstlesson;
 import java.util.Scanner;
 
 public class TestClass {
-        public static void main(String[] args) {
-            Scanner TT = new Scanner(System.in);
-            System.out.print("number of hours worked: ");
-            int a = TT.nextInt();
-            System.out.print("hourly pay rent: ");
-            int b = TT.nextInt();
-            int c = a*b;
-            System.out.print("total value: ");
-            System.out.println(c);
-
-        }
+    public static void main(String[] args) {
+//            Scanner TT = new Scanner(System.in);
+//            System.out.print("number of hours worked: ");
+//            int a = TT.nextInt();
+//            System.out.print("hourly pay rent: ");
+//            int b = TT.nextInt();
+//            int c = a*b;
+//            System.out.print("total value: ");
+//            System.out.println(c);
+        sub();
     }
+
+    public static void sub() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("number of hours worked: ");
+        int a = scanner.nextInt();
+        System.out.print("number of hours worked: ");
+        int b = scanner.nextInt();
+        int c = a / b;
+        System.out.println(c);
+
+    }
+}
